761680b5c1c825f4e9f9b00f6852f082cd815f27
SFYLL <santiagoflood@hotmail.fr>
2023-09-02 20:39:30 +0200

    update to vue3

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VuexPersist from 'vuex-persist'
+import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
-const persist = new VuexPersist({
-  key: 'state',
-  storage: window.localStorage
-})
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default createStore({
   plugins: [
-    persist.plugin
+    createPersistedState({
+      key: 'state',
+      storage: window.localStorage
+    })
   ],
   state: {
     dark: window.matchMedia('(prefers-color-scheme: dark)').matches,
@@ -19,25 +14,25 @@ export default new Vuex.Store({
   },
   mutations: {
     switchTheme(state) {
-      state.dark = !state.dark
-      if (window.matchMedia('(prefers-reduced-motion: reduce').matches) {
+      state.dark = !state.dark;
+      if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
         return document.body.classList.replace((state.dark ? 'light' : 'dark'), (state.dark ? 'dark' : 'light'))
       }
 
-      let transitionClass = (state.dark ? 'light-to-dark' : 'dark-to-light')
-      document.body.classList.add(transitionClass)
-      document.body.classList.remove((state.dark ? 'light': 'dark'))
+      let transitionClass = (state.dark ? 'light-to-dark' : 'dark-to-light');
+      document.body.classList.add(transitionClass);
+      document.body.classList.remove((state.dark ? 'light' : 'dark'));
       setTimeout(() => {
-        document.body.classList.add((state.dark ? 'dark': 'light'))
-        document.body.classList.remove(transitionClass)
-      }, 600)
+        document.body.classList.add((state.dark ? 'dark' : 'light'));
+        document.body.classList.remove(transitionClass);
+      }, 600);
     },
     toggleMobileNav(state, payload) {
       state.showMobileNav = payload;
-    },
+    }
   },
   actions: {
   },
   modules: {
   }
-})
+});
