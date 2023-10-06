da5c1b63a45fd86fdda0a400c24709f1706285f7
SFYLL <santiagoflood@hotmail.fr>
2023-10-06 16:40:57 +0200

    vue config prerender

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,37 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [{ from: 'CNAME', to: 'CNAME', toType:"file" }]
-      })
-    ]
-  }
+const plugins = [];
+if (process.env.NODE_ENV === 'production') {
+  const fs = require('fs');
+  const path = require('path');
+  const PrerenderPlugin = require('prerender-spa-plugin-next');
+
+  const metaData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'public/blog_post/meta_data.json'), 'utf-8'));
+  const postRoutes = metaData.map(post => `/blog/${post.slug}`);
+
+  const staticRoutes = ['/', '/home', '/about', '/contact', '/blog'];
+
+  const prerenderRoutes = staticRoutes.concat(postRoutes);
+
+  plugins.unshift(
+    new PrerenderPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: prerenderRoutes, 
+    })
+  );
 }
+
+plugins.unshift(
+  new CopyWebpackPlugin({
+    patterns: [{ from: 'CNAME', to: 'CNAME', toType:"file" }]
+  })
+)
+
+module.exports = {
+    transpileDependencies: true,
+    configureWebpack(config) {
+        config.plugins = [...config.plugins, ...plugins];
+    },
+  }
+
+
