a20db24e741117f79489aa7429adfb1532d6e220
SFYLL <sd@seismic.systems>
2025-06-26 11:31:59 +0200

    add exp

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,8 @@
             <h2 class="subheading">Open Source Work</h2>
             <div>
                 <ul class="list">
-                <li><a href="https://github.com/SeismicSystems" target="_blank"><code>seismic</code></a>: Encrypted evm</li>
+                <li><a href="https://github.com/SeismicSystems" target="_blank"><code>Seismic</code></a>: encrypted evm</li>
+                <li><a href="https://github.com/sfyll/pilot-ryo" target="_blank"><code>Seismic &lt;&gt; Dojo</code></a>: zk and homomorphism: shielded uniswap pools</li>
                 <li><a href="https://github.com/sfyll/OrdinArb" target="_blank"><code>ordinArb</code></a>: bitcoin dark forest explorer</li>
                 <li><a href="https://github.com/orgs/anon-abuse/repositories" target="_blank"><code>anon-abuse</code></a>: zkhack 11/23 award-winning middleware to trustlessly create or extend semaphore groups</li>
                 <li><a href="https://pull.sfyl.xyz" target="_blank"><code>selecte-application</code></a>: selectively disclose content. a pull-driven and anonymous process</li>
